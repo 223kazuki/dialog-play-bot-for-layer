3ae647ba76f9370449f6a7c445295492854676dc
Kazuki Tsutsumi <kazukitsutsumi@TRCs-iMac.local>
2017-10-24 16:33:59 -0700

    Modified.

--- src/dialog_play_bot_for_layer/handler/callback.clj
+++ src/dialog_play_bot_for_layer/handler/callback.clj
@@ -69,14 +69,83 @@
                                                        :selection_mode "none"
                                                        :items flights})})
     "OK"))
+(defmethod custome-behavier "airline/confirm" [params {:keys [layer yelp token-manager
+                                                              conversation-id] :as opts}]
+  (let [{:keys [flight-id]} params
+        _ (println params) ;; log
+        flights [{:id "9b8810c8-18e8-4c8f-99ce-2a96915a21ab"
+                  :selectable false
+                  :date "12/30(月)"
+                  :routes [{:seats "○"
+                            :flightName "TL002"
+                            :depart {:airport "HND"
+                                     :airportJapanese "羽田"
+                                     :dateTime "19:45"}
+                            :arrival {:airport "SFO"
+                                      :airportJapanese "サンフランシスコ"
+                                      :dateTime "12:00"}}]
+                  :price "￥98000"
+                  :time "9時間 15分"
+                  :milage "1539マイル"}]]
+    (layer/post-message layer conversation-id {:mime_type "application/x.card.flight.ticket.list+json"
+                                               :body (json/write-str
+                                                      {:title ""
+                                                       :subtitle ""
+                                                       :selection_mode "none"
+                                                       :items flights})})
+    "OK"))
+(defmethod custome-behavier "airline/seats" [params {:keys [layer yelp token-manager
+                                                            conversation-id] :as opts}]
+  (let [{:keys [flight-id]} params
+        _ (println params) ;; log
+        seats []]
+    (layer/post-message layer conversation-id {:mime_type "application/x.card.flight.seat+json"
+                                               :body (json/write-str
+                                                      {:title ""
+                                                       :subtitle ""
+                                                       :selection_mode "none"
+                                                       :seats seats})})
+    "OK"))
+(defmethod custome-behavier "airline/purchase" [params {:keys [layer yelp token-manager
+                                                               conversation-id] :as opts}]
+  (let [{:keys []} params
+        _ (println params) ;; log
+        order {:price "￥98,000"
+               :tax "￥0"
+               :amount "￥98,000"
+               :date "12/30(月)"
+               :confirmed false}]
+    (layer/post-message layer conversation-id {:mime_type "application/x.card.flight.ticket.purchase+json"
+                                               :body (json/write-str
+                                                      {:title ""
+                                                       :subtitle ""
+                                                       :selection_mode "none"
+                                                       :order order})})
+    "OK"))
 (defmethod custome-behavier :default [params opts] "OK")
 
 (defn dialog-play-to-layer
-  [{:keys [dialog-play layer token-manager] :as opts} conversation-id message]
+  [{:keys [dialog-play layer token-manager] :as opts} conversation-id mime-type message]
   (let [channel-uuid (or (token-manager/get-dialog-play-channel-id token-manager conversation-id)
                          (let [uuid (dialog-play/create-channel dialog-play)]
                            (token-manager/new-conversation token-manager conversation-id uuid)
                            uuid))
+        _ (println message)
+        _ (println mime-type)
+        message (case mime-type
+                  "application/x.card.flight.ticket.list+json" (-> message
+                                                                   (json/read-str :key-fn keyword)
+                                                                   :items
+                                                                   first
+                                                                   :id)
+                  "application/x.card.flight.seat+json" (as-> message m
+                                                          (json/read-str m :key-fn keyword)
+                                                          (:seats m)
+                                                          (map :name m)
+                                                          (str/join m))
+                  "application/x.card.flight.ticket.purchase+json" ""
+                  message)
+        _ (println message)
         dialog-play-messages (dialog-play/post-message dialog-play message channel-uuid)]
     (when dialog-play-messages
       (doall
@@ -94,10 +163,9 @@
 
 (defmulti  handle-webhook (fn [req opts] (get-in req [:headers "layer-webhook-event-type"])))
 (defmethod handle-webhook "Message.created" [req {:keys [dialog-play layer token-manager sync] :as opts}]
-  (let [{:keys [body params]} req]
+  (let [{:keys [body mime_type params]} req]
     (when body
       (let [body (some-> body slurp (json/read-str :key-fn keyword))
-            _ (println "body: " body)
             part (some-> body
                          (get-in [:message :parts])
                          first)
@@ -114,8 +182,8 @@
         (println "Message created: " conversation-id sender-id message mime-type)
         (when (not= sender-id (layer/get-bot-user-id layer))
           (if sync
-            (dialog-play-to-layer opts conversation-id message)
-            (letfn [(f [] (dialog-play-to-layer opts conversation-id message))]
+            (dialog-play-to-layer opts conversation-id mime-type message)
+            (letfn [(f [] (dialog-play-to-layer opts conversation-id mime-type message))]
               (.start (Thread. f)))))))))
 (defmethod handle-webhook "Conversation.created" [req {:keys [dialog-play layer token-manager sync] :as opts}]
   (let [{:keys [body params]} req]
